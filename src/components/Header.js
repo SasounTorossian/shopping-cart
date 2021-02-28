b4becc363c3cd9597f732ccb9fd3865b8822b44a
Sasoun Torossian <sasountorossian@gmail.com>
2021-02-28 15:36:47 +0000

    Added styling to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,30 @@
 import React from 'react'
-
 import {Link} from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Header = () => {
     return (
         <div className="Header">
-            <h2>MY SHOPPING SITE</h2>
+            <h1 className="nav-title">The Collection</h1>
             <nav className="nav-bar">
                 <Link to="/">
-                    <div className="nav-link">Home Page</div>
+                    <h3 className="nav-link">Home</h3>
                 </Link>
                 <Link to="/shoppingpage">
-                    <div className="nav-link">Shopping Page</div>
+                    <h3 className="nav-link">Shopping</h3>
                 </Link>
                 <Link to="">
-                    <div className="nav-link">C</div>   
+                    <h3 className="nav-link">Search</h3>   
+                </Link>
+                <Link to="">
+                    <div className="basket-container">
+                        <h3 className="nav-link">Basket</h3>   
+                        <div className="basket-number">2</div>
+                    </div>
+                </Link>
+                <Link to="">
+                    <FontAwesomeIcon icon={faGithub} className="nav-link git-link"/>
                 </Link>
             </nav>
         </div>
