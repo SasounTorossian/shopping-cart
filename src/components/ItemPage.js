50a37dc5e38cfc93843c0f3e0d82a31882ef66a5
Sasoun Torossian <sasountorossian@gmail.com>
2021-03-11 10:27:54 +0000

    Added quanity selector next to basket.

--- src/components/ItemPage.js
+++ src/components/ItemPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import inventory from "./inventory"
 
 const ItemPage = ({onAddBasket, match: {params}}) => {
@@ -26,6 +26,16 @@ const ProductDisplay = ({ item }) => {
 }
 
 const BuyBar = ({ item, collection, onAddBasket }) => {
+    const [quantity, setQuantity] = useState(1)
+
+    const handleInputChange = (value) => { if(Number(value) > 0) setQuantity(Number(value)) }
+
+    const handleDecrement = () => { if(quantity > 1) setQuantity(quantity - 1) }
+
+    const handleIncrement = () => { 
+        //TODO: If (quantity < item.stock)
+        setQuantity(quantity + 1)
+    }
 
     const handleAddBasket = (item) => { onAddBasket(item) }
 
@@ -45,6 +55,15 @@ const BuyBar = ({ item, collection, onAddBasket }) => {
                 </div>
             </div>
             <div className="buy-bar-button-container">
+                <div className="buy-bar-incrementer">
+                    <div className="buy-bar-incrementer-subtract noSelect" onClick={handleDecrement}>
+                        -
+                    </div>
+                    <input type="number" className="buy-bar-incrementor-input" value={quantity} onChange={(e) => handleInputChange(e.target.value)}></input>
+                    <div className="buy-bar-incrementer-add noSelect" onClick={handleIncrement}>
+                        +
+                    </div>
+                </div>
                 <div 
                     className={`buy-bar-button ${addClassFromCollection()}`} 
                     onClick={() => handleAddBasket(item)}
