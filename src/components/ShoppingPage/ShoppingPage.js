86859a933af657cf09b4870ead8d52fc6b0b5b39
Sasoun Torossian <sasountorossian@gmail.com>
2021-03-20 10:15:44 +0000

    Change ShoppingPage to use NavLinks.

--- src/components/ShoppingPage/ShoppingPage.js
+++ src/components/ShoppingPage/ShoppingPage.js
@@ -1,6 +1,6 @@
 import "./ShoppingPage.css"
 import React from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, NavLink } from 'react-router-dom'
 import inventory from "../inventory"
 
 // Shopping page component which renders all the items on sale.
@@ -20,24 +20,31 @@ const ShoppingPage = () => {
 const SideBar = ({ collection }) => {
     // Changes background of sidebar based on current collection.
     const backgroundFromCollection = () => { return `side-bar-${collection}-active`}
-
-    // Highlights appropriate link in side bar if it is the currently selected collection.
-    const ifRedCollection = () => { if(collection === "red") return "side-bar-link-red-active" }
-    const ifGreyCollection = () => { if(collection === "grey") return "side-bar-link-grey-active" }
-    const ifGoldCollection = () => { if(collection === "gold") return "side-bar-link-gold-active" }
     
     return (
         <nav className={`side-bar ${backgroundFromCollection()}`}>
             <div className="side-bar-link-container">
-                <Link to="/shoppingpage/red" className={`side-bar-link ${ifRedCollection()}`}>
+                <NavLink 
+                    to="/shoppingpage/red" 
+                    className="side-bar-link"
+                    activeClassName="side-bar-link-red-active"
+                >
                     <p className="side-bar-text side-bar-text-red">Red Collection</p>
-                </Link>
-                <Link to="/shoppingpage/grey" className={`side-bar-link ${ifGreyCollection()}`}>
+                </NavLink>
+                <NavLink 
+                    to="/shoppingpage/grey" 
+                    className="side-bar-link"
+                    activeClassName="side-bar-link-grey-active"
+                >
                     <p className="side-bar-text side-bar-text-grey">Grey Collection</p>
-                </Link>
-                <Link to="/shoppingpage/gold" className={`side-bar-link ${ifGoldCollection()}`}>
+                </NavLink>
+                <NavLink 
+                    to="/shoppingpage/gold" 
+                    className="side-bar-link"
+                    activeClassName="side-bar-link-gold-active"
+                >
                     <p className="side-bar-text side-bar-text-gold">Gold Collection</p>
-                </Link>
+                </NavLink>
             </div>
         </nav>
     )
